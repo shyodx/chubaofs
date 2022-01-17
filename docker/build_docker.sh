72b20b02b285ed25b9ae214e989a33e6b047b4ac
Shuoran Liu <shuoranliu@gmail.com>
2022-01-17 18:31:11 +0800

    ci: use go 1.16.12 instead of go 1.15
    As of Go 1.16, os.ReadDir is a more efficient and correct choice than
    ioutil.ReadDir, so we are making the updates.
    
    Signed-off-by: Shuoran Liu <shuoranliu@gmail.com>

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 RootPath=$(cd $(dirname $0) ; pwd)
-CfsBase="ghcr.io/chubaofs/cbfs-base:1.1"
+CfsBase="ghcr.io/chubaofs/cbfs-base:1.1-golang-1.16.12"
 
 docker build -t ${CfsBase} -f ${RootPath}/Dockerfile ${RootPath}
