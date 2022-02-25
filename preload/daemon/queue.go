fcc35613c2c585712a588996cb4d718ec792a645
shengyong002 <shengyong002@ke.com>
2022-02-25 11:09:32 +0800

    preload: Introduce queues and friends
    A queue is used to transmit file operation requests and results
    between user app and preload daemon. There are three queues:
      * CtrlQueue: is consist of queue header and CtrlItems. Each file
                   operation has one CtrlItem, which may have several
                   DataItems, and must have only one DoneItem to save
                   return value. File operations and its parameters
                   should be put in CtrlItem.
      * DataQueue: contains only DataItems. Multiple DataItems are
                   chained by its Next element. And value DataSlotEnd
                   indicates this is the last DataItem of the chain.
      * DoneQueue: contains only DoneItems. It saves return value,
                   which is a signed 64-bit value.
    
    Every task have a QueueArray which contains above three queues.
    Each queue shares a chunk of memory between user app and preload
    daemon to avoid extra memory copy.

--- preload/daemon/queue.go
+++ preload/daemon/queue.go
@@ -33,6 +33,161 @@ const (
 	CmdWakeup
 )
 
+type QueueType int
+
+const (
+	CtrlQueue QueueType = iota
+	DataQueue
+	DoneQueue
+	MaxQueue
+)
+
+const (
+	// ctrl related
+	CtrlItemSize = 64
+	CtrlDataSize = 40
+
+	CtrlStateNew        = 0
+	CtrlStateReady      = 0x00000001
+	CtrlStateDataItem   = 0x10000000
+	CtrlStateInlineData = 0x20000000
+
+	// done related
+	DoneItemSize = 24
+
+	DoneStateNew   = 0
+	DoneStateReady = 1
+	DoneStateStale = 2
+
+	// data related
+	DataItemSize = 4096
+	DataDataSize = 4080 //4076
+
+	DataSlotEnd uint32 = ^uint32(0)
+)
+
+type CtrlItem struct {
+	ReqId   uint64             // unique id for request
+	OpCode  uint32             //
+	State   uint32             // atomic: DATA_ITEM | INLINE_DATA
+	DoneIdx uint32             // where to put return value
+	DataIdx uint32             // start from this slot in data queue
+	data    [CtrlDataSize]byte // for short params and da
+}
+
+type DoneItem struct {
+	ReqId  uint64 // the same with ctrl_item
+	OpCode uint32
+	State  uint32 // atomic: FRESH or STALE
+	RetVal int64
+}
+
+// DataItem is a chain, Next indicates the position of the next item
+type DataItem struct {
+	ReqId  uint64 // the same with ctrl_item
+	OpCode uint32
+	Next   uint32 // next data item, 0xffffffff means end of chain
+	data   [DataDataSize]byte
+}
+
+func (item *CtrlItem) String() string {
+	str := fmt.Sprintf("ReqId:%v OpCode:%v State:%x DoneIdx:%v DataIdx:%v",
+		item.ReqId, item.OpCode, item.State, item.DoneIdx, item.DataIdx)
+	if item.State&CtrlStateInlineData == CtrlStateInlineData {
+		str = fmt.Sprintf("CtrlItem(%s Data:%s)", str, string(item.data[:]))
+	} else {
+		str = fmt.Sprintf("CtrlItem(%s Data:<in data item>)", str)
+	}
+	return str
+}
+
+func (item *DoneItem) String() string {
+	return fmt.Sprintf("DoneItem(ReqId:%v OpCode:%v State:%x RetVal:%v)",
+		item.ReqId, item.OpCode, item.State, item.RetVal)
+}
+
+func (item *DataItem) String() string {
+	return fmt.Sprintf("DataItem(ReqId:%v OpCode:%v Next:%x",
+		item.ReqId, item.OpCode, item.Next)
+}
+
+const QueueSuspendFlag uint32 = 0x1
+
+type QueueHeader struct {
+	head     uint32
+	tail     uint32
+	flags    uint32
+	reserved uint32
+}
+
+type QueueHeaderPtr struct {
+	head  *uint32
+	tail  *uint32
+	flags *uint32
+}
+
+// An QueueInfo is based on share memory object. It behaves like a ring buffer
+// of a bunch of slots. Each slot has an equal size.
+type QueueInfo struct {
+	queueType QueueType
+	// shmFile is a shared memory object opened by shm_open, the object
+	// path is registered by app
+	//shmFile *os.File
+	// size is the size of a slot
+	size uint16
+	// members is the number of slots
+	members uint32
+	mask    uint32
+	// head is the index of current write position
+	head uint32
+	// tail is the index of current read position
+	tail uint32
+	// filename is the name of shmFile
+	namelen  uint16
+	filename []byte
+
+	// data is base address of shared memory mapped by shared memory object
+	addr []byte
+	hdr  QueueHeaderPtr
+	data []byte
+}
+
+// An QueueArray represents a direct connection between app and FUSE daemon
+type QueueArray struct {
+	// ctrl is a read-only queue which saves command request from app
+	ctrl *QueueInfo
+	// data is a read-only queue which saves request data from app, it is
+	// indexed by command request
+	data *QueueInfo
+	// done is a write-only queue which saves done command request
+	done *QueueInfo
+}
+
+func parseQueueInfo(data []byte) (*QueueInfo, int) {
+	var start, end int
+
+	queue := &QueueInfo{}
+
+	/* FIXME: need check validation of input params */
+	end = 2
+	queue.namelen = binary.BigEndian.Uint16(data[start:end])
+	start = end
+	end += 2
+	queue.size = binary.BigEndian.Uint16(data[start:end])
+	start = end
+	end += 4
+	queue.members = binary.BigEndian.Uint32(data[start:end])
+	queue.mask = queue.members - 1
+	//log.LogDebugf("namelen %v size %v members %v", queue.namelen, queue.size, queue.members)
+	fmt.Printf("namelen %v size %v members %v end %v\n", queue.namelen, queue.size, queue.members, end+int(queue.namelen))
+	queue.filename = make([]byte, queue.namelen)
+	start = end
+	end += int(queue.namelen)
+	copy(queue.filename, data[start:end])
+
+	return queue, end
+}
+
 func handleRegisterNew(data []byte) (int, []byte) {
 	var (
 		idStr []byte = make([]byte, 8) // 8 is enough for a 64bit value
