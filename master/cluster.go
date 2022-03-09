shyodx/chubaofs

commit 863be6d8829d16d6d8dfced61ba86023a3ce5ed7

fix: Refactor finding partitins to be offlined
There is not need to check if `targetAddr' is set or not every time. Add
if we are tryting to decommission a node, there is no need to copy all
partitions in this node to a new slice.

Signed-off-by: shengyong002 <shengyong002@ke.com>

master/cluster.go
@@ -988,6 +988,8 @@ func (c *Cluster) getAllMetaPartitionsByMetaNode(addr string) (partitions []*Met
 }
 
 func (c *Cluster) migrateDataNode(srcAddr, targetAddr string, limit int) (err error) {
+	var toBeOffLinePartitions []*DataPartition
+
 	msg := fmt.Sprintf("action[migrateDataNode], src(%s) migrate to target(%s) cnt(%d)", srcAddr, targetAddr, limit)
 	log.LogWarn(msg)
 
@@ -1000,14 +1002,18 @@ func (c *Cluster) migrateDataNode(srcAddr, targetAddr string, limit int) (err er
 	defer src.MigrateLock.Unlock()
 
 	partitions := c.getAllDataPartitionByDataNode(src.Addr)
-	toBeOffLinePartitions := make([]*DataPartition, 0)
-	for _, dp := range partitions {
-		// two replica can't exist on same node
-		if targetAddr != "" && dp.hasHost(targetAddr) {
-			continue
-		}
+	if targetAddr != "" {
+		toBeOffLinePartitions = make([]*DataPartition, 0)
+		for _, dp := range partitions {
+			// two replica can't exist on same node
+			if dp.hasHost(targetAddr) {
+				continue
+			}
 
-		toBeOffLinePartitions = append(toBeOffLinePartitions, dp)
+			toBeOffLinePartitions = append(toBeOffLinePartitions, dp)
+		}
+	} else {
+		toBeOffLinePartitions = partitions
 	}
 
 	if len(toBeOffLinePartitions) <= 0 && len(partitions) != 0 {
@@ -1513,6 +1519,8 @@ func (c *Cluster) getBadDataPartitionsView() (bpvs []badPartitionView) {
 }
 
 func (c *Cluster) migrateMetaNode(srcAddr, targetAddr string, limit int) (err error) {
+	var toBeOfflineMps []*MetaPartition
+
 	msg := fmt.Sprintf("action[migrateMetaNode],clusterID[%v] migrate from Node[%v] to [%s] begin", c.Name, srcAddr, targetAddr)
 	log.LogWarn(msg)
 
@@ -1525,13 +1533,17 @@ func (c *Cluster) migrateMetaNode(srcAddr, targetAddr string, limit int) (err er
 	defer metaNode.MigrateLock.Unlock()
 
 	partitions := c.getAllMetaPartitionByMetaNode(srcAddr)
-	toBeOfflineMps := make([]*MetaPartition, 0)
-	for _, mp := range partitions {
-		if targetAddr != "" && contains(mp.Hosts, targetAddr) {
-			continue
-		}
+	if targetAddr != "" {
+		toBeOfflineMps := make([]*MetaPartition, 0)
+		for _, mp := range partitions {
+			if contains(mp.Hosts, targetAddr) {
+				continue
+			}
 
-		toBeOfflineMps = append(toBeOfflineMps, mp)
+			toBeOfflineMps = append(toBeOfflineMps, mp)
+		}
+	} else {
+		toBeOfflineMps = partitions
 	}
 
 	if len(toBeOfflineMps) <= 0 && len(partitions) != 0 {

commit 974d33b17f090811ed422e6faefd650db7d07ed6

metanode: Fix variable scope in migrateMetaNode
Fixes: 863be6d8 ("master: Remove condition check that is always false")
Signed-off-by: shengyong002 <shengyong002@ke.com>

master/cluster.go
@@ -1534,7 +1534,7 @@ func (c *Cluster) migrateMetaNode(srcAddr, targetAddr string, limit int) (err er
 
 	partitions := c.getAllMetaPartitionByMetaNode(srcAddr)
 	if targetAddr != "" {
-		toBeOfflineMps := make([]*MetaPartition, 0)
+		toBeOfflineMps = make([]*MetaPartition, 0)
 		for _, mp := range partitions {
 			if contains(mp.Hosts, targetAddr) {
 				continue