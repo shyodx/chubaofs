8023783e7dd3d54963227b4f70d70577c9e8e804
Shuoran Liu <shuoranliu@gmail.com>
2022-03-15 21:51:33 +0800

    enhance: use Readdirnames instead of Readdir
    Use Readdirnames instead of Readdir to eliminate unnecessary inode get
    system calls.
    
    Signed-off-by: Shuoran Liu <shuoranliu@gmail.com>

--- datanode/disk.go
+++ datanode/disk.go
@@ -25,10 +25,11 @@ import (
 	"syscall"
 	"time"
 
+	"os"
+
 	"github.com/chubaofs/chubaofs/proto"
 	"github.com/chubaofs/chubaofs/util/exporter"
 	"github.com/chubaofs/chubaofs/util/log"
-	"os"
 )
 
 var (
@@ -345,15 +346,21 @@ func (d *Disk) RestorePartition(visitor PartitionVisitor) {
 		partitionSize int
 	)
 
-	fileInfoList, err := os.ReadDir(d.Path)
+	fh, err := os.Open(d.Path)
+	if err != nil {
+		log.LogErrorf("action[RestorePartition] failed to open dir(%v) err(%v).", d.Path, err)
+		return
+	}
+	defer fh.Close()
+
+	fileList, err := fh.Readdirnames(0)
 	if err != nil {
 		log.LogErrorf("action[RestorePartition] read dir(%v) err(%v).", d.Path, err)
 		return
 	}
 
 	var wg sync.WaitGroup
-	for _, fileInfo := range fileInfoList {
-		filename := fileInfo.Name()
+	for _, filename := range fileList {
 		if !d.isPartitionDir(filename) {
 			continue
 		}
@@ -364,7 +371,7 @@ func (d *Disk) RestorePartition(visitor PartitionVisitor) {
 			continue
 		}
 		log.LogDebugf("acton[RestorePartition] disk(%v) path(%v) PartitionID(%v) partitionSize(%v).",
-			d.Path, fileInfo.Name(), partitionID, partitionSize)
+			d.Path, filename, partitionID, partitionSize)
 
 		if isExpiredPartition(partitionID, dinfo.PersistenceDataPartitions) {
 			log.LogErrorf("action[RestorePartition]: find expired partition[%s], rename it and you can delete it "+
